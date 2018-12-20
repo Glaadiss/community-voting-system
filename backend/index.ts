f309502cdb1b5b9660a943a4c41160a799583242
Bartłomiej Gładys (bartgla052) <bartgla052@student.polsl.pl>
2018-12-21 00:24:06 +0100

    Add graphql-yoga and basic api.

--- backend/index.ts
+++ backend/index.ts
@@ -1,31 +1,37 @@
-import { prisma } from './generated/prisma-client';
+import { GraphQLServer } from 'graphql-yoga';
+import { prisma, Prisma } from './generated/prisma-client';
 
-// A `main` function so that we can use async/await
-async function main() {
-  // Create a new user called `Alice`
-  const newUser = await prisma.createUser({ name: 'Alice' });
-  const project = await prisma.createProject({ name: 'basicProject' });
-  const contest = await prisma.createContest({ name: 'basicContest' });
-  // tslint:disable-next-line:no-console
-  // console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`);
+const resolvers = {
+  Query: {
+    contests(root, args, context: { prisma: Prisma }) {
+      return context.prisma.contests();
+    },
+    contest(root, args, context: { prisma: Prisma }) {
+      return context.prisma.contest({ id: args.contestId });
+    },
+  },
+  Mutation: {
+    createContest(root, args, context: { prisma: Prisma }) {
+      return context.prisma.createContest({
+        name: args.name,
+      });
+    },
+    // addProjectsToContest(root, args, context: {prisma: Prisma}) {
+    //   return context.prisma.upda;
+    // },
+    createUser(root, args, context: { prisma: Prisma }) {
+      return context.prisma.createUser({ name: args.name });
+    },
+  },
+};
 
-  // Read all users from the database and print them to the console
-  const allUsers = await prisma.users();
-  const allProjects = await prisma.projects();
-  const allContests = await prisma.contests();
-
-  // tslint:disable-next-line:no-console
-  console.log(allUsers);
-  // tslint:disable-next-line:no-console
-  console.log(allProjects);
-  // tslint:disable-next-line:no-console
-  console.log(allContests);
-}
-
-// const postsByUser = await prisma
-// .user({ email: "bob@prisma.io" })
-// .posts()
-// console.log(`All posts by that user: ${JSON.stringify(postsByUser)}`)
+const server = new GraphQLServer({
+  typeDefs: './schema.graphql',
+  resolvers,
+  context: {
+    prisma,
+  },
+});
 
 // tslint:disable-next-line:no-console
-main().catch(e => console.error(e));
+server.start(() => console.log('Server is running on http://localhost:4000'));
