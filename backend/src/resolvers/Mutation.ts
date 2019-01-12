8b1a02d33c0d26df88fba1ce776c1c42da9cd126
pkramarczyk <pkramarczyk@users.noreply.github.com>
2019-01-12 23:59:01 +0100

    Added validation for createUser and login mutations

--- backend/src/resolvers/Mutation.ts
+++ backend/src/resolvers/Mutation.ts
@@ -2,10 +2,8 @@ import bcrypt = require('bcrypt');
 import { prisma } from '../../prisma/generated/prisma-client';
 import { BadData } from '../errorTypes';
 import { Context, ROLE } from '../utils/customTypes';
-import {
-    allowAdmin,
-    sign
-} from '../services/auth';
+import { allowAdmin, sign } from '../services/auth';
+import { validateEmail, validatePassword } from '../utils/validator';
 
 const Mutation = {
     createContest(_, args, context: Context) {
@@ -14,9 +12,35 @@ const Mutation = {
         });
     },
     async createUser(_, { data }, context: Context) {
+        if (!validatePassword(data.password))
+            throw new BadData({
+                data: {
+                    additional_info: 'Password must have at least 8 characters and contain: 1 upper case character, 1 lower case characterm, 1 number, 1 special character.'
+                }
+            })
+        if (!validateEmail(data.email))
+            throw new BadData({
+                data: {
+                    additional_info: 'Invalid email address.'
+                }
+            })
+        const emailExists = await prisma.user({ email: data.email });
+        if (emailExists)
+            throw new BadData({
+                data: {
+                    additional_info: 'User with this email already exists.'
+                }
+            });
+        const peselExists = await prisma.user({ pesel: data.pesel })
+        if (peselExists)
+            throw new BadData({
+                data: {
+                    additional_info: 'User with this pesel number already exists.'
+                }
+            });
         const passwordHash = await bcrypt.hash(data.password, 5);
         const user = await prisma.createUser({
-            email: data.email,
+            email: data.email.toLowerCase(),
             name: data.name,
             postalCode: data.postalCode,
             pesel: data.pesel,
@@ -29,15 +53,22 @@ const Mutation = {
     async login(_, { data }, context: Context) {
         const user = await prisma.user({ email: data.email });
         if (!user) {
-            throw new BadData();
+            throw new BadData({
+                data: {
+                    additional_info: 'Invalid credentials.'
+                }
+            });
         }
         const passwordCorrect = await bcrypt.compare(
             data.password,
             user.passwordHash,
         );
-        if (!passwordCorrect) {
-            throw new BadData();
-        }
+        if (!passwordCorrect)
+            throw new BadData({
+                data: {
+                    additional_info: 'Invalid credentials.'
+                }
+            });
         const token = sign(user);
         return { token };
     }
