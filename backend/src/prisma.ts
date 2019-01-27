6aa00fff8687e6b6a5a567267aaabfade945705d
Bartłomiej Gładys (bartgla052) <bartgla052@student.polsl.pl>
2019-01-27 15:03:37 +0100

    Change default endpoints to container.

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -2,7 +2,7 @@ import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
-  endpoint: 'http://localhost:4466',
+  endpoint: 'http://35.246.149.86',
   secret: 'thisismysupersecrettext'
 });
 
