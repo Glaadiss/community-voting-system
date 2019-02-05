9efe8ac8ae9ef51aa2f50b1329bfa8a0b6002afa
Bartłomiej Gładys (bartgla052) <bartgla052@student.polsl.pl>
2019-02-05 22:25:09 +0100

    Add charts.

--- frontend/src/scenes/Scores/index.js
+++ frontend/src/scenes/Scores/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import { withStyles } from '@material-ui/core';
+import PropTypes from 'prop-types';
 import { AuthContext } from '../../App';
 import ContestScore from '../../components/ContestScore';
+
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    overflow: 'hidden',
+    alignItems: 'center',
+    backgroundColor: theme.palette.background.paper,
+  },
+  gridList: {
+    width: '70%',
+  },
+});
 
 const GET_CONTESTS = gql`
   {
@@ -13,17 +31,18 @@ const GET_CONTESTS = gql`
       projects {
         id
         title
-      }
-      votes {
-        contest {
+        votes {
           id
         }
+      }
+      votes {
+        id
       }
     }
   }
 `;
 
-function Scores() {
+function Scores({ classes }) {
   return (
     <AuthContext.Consumer>
       {context => (
@@ -31,12 +50,33 @@ function Scores() {
           {({ loading, error, data }) => {
             if (loading || !data) return null;
             if (error) return error.message;
-
-            return data.contests.map(el => (
-              <div key={el.id}>
-                <ContestScore {...el} />
+            const contests = data.contests.filter(
+              el => el.projects.length > 0 && el.votes.length > 0,
+            );
+            console.log(contests);
+            return (
+              <div className={classes.root}>
+                <GridList
+                  cellHeight={450}
+                  className={classes.gridList}
+                  cols={4}
+                  spacing={20}
+                >
+                  {contests.map(el => [
+                    <GridListTile
+                      key="lastContests"
+                      cols={1}
+                      style={{ textAlign: 'center', lineHeight: '350px' }}
+                    >
+                      <h2>{el.title}</h2>
+                    </GridListTile>,
+                    <GridListTile cols={3} style={{ textAlign: 'center' }}>
+                      <ContestScore {...el} />
+                    </GridListTile>,
+                  ])}
+                </GridList>
               </div>
-            ));
+            );
           }}
         </Query>
       )}
@@ -44,4 +84,7 @@ function Scores() {
   );
 }
 
-export default Scores;
+Scores.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+export default withStyles(styles)(Scores);
